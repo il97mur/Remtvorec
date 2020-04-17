e2602f0367e4561ba3182d120b7d3064d92ddb3e
il97mur <ilha9749@gmail.com>
2020-04-18 03:21:19 +0600

    Calculate

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -17,4 +17,28 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	  });
 	} );
+
+	$('.type-of-house__item').on('click', function(){
+		$('.type-of-house__item').removeClass('type-of-house__item_active');
+		$(this).toggleClass('type-of-house__item_active');
+	});
+
+	$('.type-of-repair__item').on('click', function(){
+		$('.type-of-repair__item').removeClass('type-of-repair__item_active');
+		$('.type-of-repair__name').removeClass('type-of-repair__name_active');
+		$(this).addClass('type-of-repair__item_active');
+		$(this).find('.type-of-repair__name').addClass('type-of-repair__name_active');
+
+		if ($(this).hasClass('j-cosmetic')) {
+			$('.j-content').removeClass('type-of-repair__text_visible');
+			$('.j-cosmetic-content').addClass('type-of-repair__text_visible');
+		} else if ($(this).hasClass('j-capital')) {
+			$('.j-content').removeClass('type-of-repair__text_visible');
+			$('.j-capital-content').addClass('type-of-repair__text_visible');
+		} else if ($(this).hasClass('j-elite')) {
+			$('.j-content').removeClass('type-of-repair__text_visible');
+			$('.j-elite-content').addClass('type-of-repair__text_visible');
+		}
+
+	})
 });
