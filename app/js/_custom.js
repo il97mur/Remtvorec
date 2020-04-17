f37292650ee94fc35dedcf07cd0e538ac4c623a8
il97mur <ilha9749@gmail.com>
2020-04-17 19:04:50 +0600

    Main screen & slider

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -2,4 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
 
+	$( function() {
+	  var handle = $( "#custom-handle" );
+	  $( "#slider" ).slider({
+		create: function() {
+		  handle.text( $( this ).slider( "value" ) );
+		},
+		range: 'min',
+		min: 20,
+		max: 200,
+		value: 154,
+		slide: function( event, ui ) {
+		  handle.text( ui.value );
+		}
+	  });
+	} );
 });
